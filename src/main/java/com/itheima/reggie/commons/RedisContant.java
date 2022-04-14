d08f2d8fc66aaccb514e30b29eb01d1a9205ceaf
della <3158424005@qq.com>
2022-04-14 12:18:20 +0800

    della

---
+++ src/main/java/com/itheima/reggie/commons/RedisContant.java
@@ -0,0 +1,8 @@
+package com.itheima.reggie.commons;
+
+public class RedisContant {
+  public static final String DISH = "dish";
+  public static final String CATEGORYDISH ="category_dish_";
+  public static final String CATEGORYALL = "categoryAll";
+
+}

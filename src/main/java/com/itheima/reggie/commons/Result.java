295e1e9042bed099dd9e37d6469cd1b390534202
della <3158424005@qq.com>
2022-04-14 12:15:06 +0800

    呀哈哈

--- src/main/java/com/itheima/reggie/commons/Result.java
+++ src/main/java/com/itheima/reggie/commons/Result.java
@@ -1,5 +1,6 @@
 package com.itheima.reggie.commons;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.Data;
@@ -9,7 +10,7 @@ import lombok.Data;
  * @param <T>
  */
 @Data
-public class Result<T> {
+public class Result<T> implements Serializable {
   private Integer code; //编码：1成功，0和其它数字为失败
   private String msg; //错误信息
   private T data; //数据
